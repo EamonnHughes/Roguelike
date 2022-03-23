48ba294db421d597fda30aab3d95d2c33fad2e77
arathorn2 <efohughes@gmail.com>
2022-03-23 17:17:53 -0400

    Update Roguelike.scala

--- src/main/scala/Roguelike.scala
+++ src/main/scala/Roguelike.scala
@@ -1,13 +1,20 @@
 import processing.awt.PGraphicsJava2D
 import processing.core._
 import processing.event.{KeyEvent, MouseEvent}
-import processing.opengl.PGraphicsOpenGL
+import processing.opengl.PGraphics3D
+
+import processing.opengl._
+
+import processing.core.PGraphics
+
+import processing.opengl.PGraphics3D
 
 class Roguelike extends PApplet {
   override def setup(): Unit = {}
   override def settings(): Unit = {
-
+    val pg = null
     size(1200, 1200)
+
   }
   override def draw(): Unit = {
 
